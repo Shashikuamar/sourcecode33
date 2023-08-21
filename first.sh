e117f5baa700f1fc9100ffa1fad2cbad89bf4c02
shashikuamar <shashikumarmandya7026@gmail.com>
2023-08-21 17:33:20 +0530

    my first shell script

---
+++ first.sh
@@ -0,0 +1,15 @@
+ This is my first shell program   <-- comment
+#Line #3 is known as shebang line
+#!/bin/bash
+
+echo "Plese enter your First Name:"
+
+read Fname
+
+echo "Plese enter your Last  Name:"
+
+read Lname
+
+echo "welcome $Fname $Lname, This is the Devops session"
+
+gi
